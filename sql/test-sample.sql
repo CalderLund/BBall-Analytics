bd26e6e4324717836d753485567c2d2e916d75a0
TianchantgZhang <lesdanita@outlook.com>
2020-02-08 18:36:45 -0500

    Added test-sample.sql, create tables directory

---
+++ sql/test-sample.sql
@@ -0,0 +1,20 @@
+-- Displaying all teams in our database since 1940's
+select distinct team_name from
+    (TeamInfo join TeamStatAndInfo on TeamInfo.team_id = TeamStatAndInfo.stats_team_id)
+where yr >= 1940
+
+--filtering teams by year
+--given input
+select distinct team_name from
+    (TeamInfo join TeamStatAndInfo on TeamInfo.team_id = TeamStatAndInfo.stats_team_id)
+where yr = <input>
+
+--user modify db
+--create account
+INSERT INTO Accounts VALUES (1, 'administrator', 'cs348family');
+
+--add Favourite Teams
+INSERT INTO FavouriteTeams VALUES(1, 'TMD', 1940);
+
+--add Favourite Player
+INSERT INTO FavouritePlayers VALUES(1, 'TMD');
